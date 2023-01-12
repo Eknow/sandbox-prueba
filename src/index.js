93d62863a1a52811834c3c0dbc735f27123fa575
Eknow <107022066+Eknow@users.noreply.github.com>
2023-01-12 11:56:40 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>UTD</h1>
+<div>
+ <p>Just only one change</p>
+  <p>You can see my works in <a href="https://github.com/Eknow/market-pos.git" target="_blank" rel="noopener noreferrer">here</a>. <p>
+</div>
+`;
